c2cfd339761e121996d03a4b2572e7bb84951125
Leman-Quinn <PabloN.A@hotmail.com>
2025-08-01 21:04:47 -0300

    general updates and cleaning

--- fifteen_en_final_corrected.py
+++ fifteen_en_final_corrected.py
@@ -1,44 +1,33 @@
-#Fifteen
 from time import sleep
 import random
 
 Q = 20
 
+def tittle():
+    print(f"+++ F  I  F  T  E  E  N +++")
 
-def intro():
-    """Displays the game tittle."""
-
-    print(f"< < < F  I  F  T  E  E  N > > >")
-
-
-def close():
-    """Exits the program after inputing any key."""
-
+def exit():
     while True:
-        exit_keyword = input("Enter any key to exit   ")
-        if exit_keyword != "":
+        key = input("Enter any key to exit")
+        if key != "":
             exit()
 
-
 def rules():
-    """Shows the rules."""
-
     print(f"\nRules: ")
-    sleep(1)
-    print(f"To be able to sort all the given randomized numbers in the last amount of movements.")
-    sleep(1)
-    print(f"Movements are defined by up, down, left and right. Additionally, several movements can be input at once.")
+    sleep(0.5)
+    print(f"Sort all the numbers in the last amount of movements.")
+    sleep(0.5)
+    print(f"Movements are up, down, left and right.")
+    sleep(0.5)
+    print(f"You can also queue multiple inputs at once!")
     main()
 
-
-def yassifier():
-    """"Beauty is also important, queen. Periot."""
-
+def flair():
     sleep(0.85)
     print("\nGenerating board . . .",)
-    sleep(2)
-    print("\nScrambling numbers . . .")
     sleep(1)
+    print("\nScrambling numbers . . .")
+    sleep(2)
     print("\nPerforming complex ecuations and calculations . .")
     sleep(0.374)
     print("\nSolutions found in 0.374 seconds. Record efficiency detected.")
@@ -47,99 +36,21 @@ def yassifier():
     sleep(0.5)       
     print("(Mandatory)")
     print("\n")
-    sleep(2)
+    sleep(1)
 
-
-def printer(board, rows, columns):
-    """Formats and displays the board according to the parameters given."""
-
+def printBoard(board, rows, columns):
     for row in range(int(rows)): 
         for column in range(int(columns)):
             print(f"{board[row][column]:^3}", end=" ")
         print(" ")
 
-
-def move_up(board, rows, columns):
-    """Moves the zero upwards."""
-
-    zero_i = None
-    zero_j = None
-
-    for i in range(int(rows)): #finds coordinate i and j for the zero
-        for j in range(int(columns)):
-            if board[i][j]==0:
-                zero_i = i
-                zero_j = j
-            else:
-                continue
-
-    if (zero_i-1)>=0: #avoids going out of list range
-        board[zero_i][zero_j], board[zero_i-1][zero_j] = board[zero_i-1][zero_j], board[zero_i][zero_j] #performs the switch
-
-
-def move_down(board, rows, columns):
-    """Moves the zero downwards."""
-
-    zero_i = None
-    zero_j = None
-
-    for i in range(int(rows)): #finds the i and j coordinate for the zero
-        for j in range(int(columns)):
-            if board[i][j]==0:
-                zero_i = i
-                zero_j = j
-            else:
-                continue
-
-    if (zero_i+1)<int(rows): #avoids going out of list range
-        board[zero_i][zero_j], board[zero_i+1][zero_j] = board[zero_i+1][zero_j], board[zero_i][zero_j] #performs the switch
-
-
-def move_left(board, rows, columns):
-    """Moves the zero leftward."""
-
-    zero_i = None
-    zero_j = None
-
-    for i in range(int(rows)): #finds the i and j coordinate for the zero
-        for j in range(int(columns)):
-
-            if board[i][j]==0:
-                zero_i = i
-                zero_j = j
-            else:
-                continue
-
-    if (zero_j-1)>=0: #avoids going out of list range
-        board[zero_i][zero_j], board[zero_i][zero_j-1] = board[zero_i][zero_j-1], board[zero_i][zero_j] #performs the switch
-
-
-def move_right(board, rows, columns):
-    """Moves the zero rightward."""
-
-    zero_i = None
-    zero_j = None
-
-    for i in range(int(rows)): #finds the i and j coordinate for the zero
-        for j in range(int(columns)):
-            if board[i][j]==0:
-                zero_i = i
-                zero_j = j
-            else:
-                continue
-
-    if (zero_j+1)<int(columns): #avoids going out of list range
-        board[zero_i][zero_j], board[zero_i][zero_j+1] = board[zero_i][zero_j+1], board[zero_i][zero_j] #performs the switch
-
-
-def board_generator(total_pieces, num_rows, num_columns):
-    """Generates the playable board(dynamic) and winner board(static)."""
-    
+def generateBoards(total_pieces, num_rows, num_columns):
+    #Generates the playable board and winner board
     current_number = 1 #how the numbers are assigned when generating the board
-    board = []
+    play_board = []
     win_board = []
 
-    for rows in range(int(num_rows)): #generates the board
+    for rows in range(int(num_rows)):
                 rows = []
                 for columns in range(int(num_columns)):
                     if current_number < total_pieces:
@@ -147,12 +58,11 @@ def board_generator(total_pieces, num_rows, num_columns):
                         current_number += 1
                     else:
                         rows.append(0)
-                board.append(rows)
+                play_board.append(rows)
                 win_board.append(list(rows))
-    return board, win_board
+    return play_board, win_board
 
-
-def randomizer(board, num_rows, num_columns):
+def randomizeBoard(board, num_rows, num_columns):
     """Randomizes the playable board(dynamic)"""
 
     for i in range(Q):
@@ -168,6 +78,66 @@ def randomizer(board, num_rows, num_columns):
         else:
             move_left(board, num_rows, num_columns)    
 
+def move_up(board, rows, columns):
+    zero_i = None
+    zero_j = None
+
+    for i in range(int(rows)): #finds coordinate i and j for the zero
+        for j in range(int(columns)):
+            if board[i][j]==0:
+                zero_i = i
+                zero_j = j
+            else:
+                continue
+
+    if (zero_i-1)>=0: #avoids going out of list range
+        board[zero_i][zero_j], board[zero_i-1][zero_j] = board[zero_i-1][zero_j], board[zero_i][zero_j] #performs the switch
+
+def move_down(board, rows, columns):
+    zero_i = None
+    zero_j = None
+
+    for i in range(int(rows)): #finds the i and j coordinate for the zero
+        for j in range(int(columns)):
+            if board[i][j]==0:
+                zero_i = i
+                zero_j = j
+            else:
+                continue
+
+    if (zero_i+1)<int(rows): #avoids going out of list range
+        board[zero_i][zero_j], board[zero_i+1][zero_j] = board[zero_i+1][zero_j], board[zero_i][zero_j] #performs the switch
+
+def move_left(board, rows, columns):
+    zero_i = None
+    zero_j = None
+
+    for i in range(int(rows)): #finds the i and j coordinate for the zero
+        for j in range(int(columns)):
+
+            if board[i][j]==0:
+                zero_i = i
+                zero_j = j
+            else:
+                continue
+
+    if (zero_j-1)>=0: #avoids going out of list range
+        board[zero_i][zero_j], board[zero_i][zero_j-1] = board[zero_i][zero_j-1], board[zero_i][zero_j] #performs the switch
+
+def move_right(board, rows, columns):
+    zero_i = None
+    zero_j = None
+
+    for i in range(int(rows)): #finds the i and j coordinate for the zero
+        for j in range(int(columns)):
+            if board[i][j]==0:
+                zero_i = i
+                zero_j = j
+            else:
+                continue
+
+    if (zero_j+1)<int(columns): #avoids going out of list range
+        board[zero_i][zero_j], board[zero_i][zero_j+1] = board[zero_i][zero_j+1], board[zero_i][zero_j] #performs the switch
 
 def main():
         """Main body that houses the structure and overall functions of the program itself."""
@@ -177,22 +147,22 @@ def main():
             num_rows = input("How many rows should the board have?   ")
             num_columns = input("And what about columns?    ")
             total_pieces = int(num_rows) * int(num_columns)
-            board, win_board = board_generator(total_pieces, num_rows, num_columns)
+            board, win_board = generateBoards(total_pieces, num_rows, num_columns)
             moves_counter = 0
             
             if num_rows == "1" or num_columns == "1": #avoids creating rows and/or columns of length 1
                 print("\nThe number of rows and/or columns must be higher than 1.")
                 return main()
             
-            yassifier() #flavor
-            randomizer(board, num_rows, num_columns)
-            printer(board, num_rows, num_columns)
+            flair() #flavor
+            randomizeBoard(board, num_rows, num_columns)
+            printBoard(board, num_rows, num_columns)
 
             while True:
                 if board == win_board: #checks for winning conditions
                     print(f"Congrats! You've won in a total of {moves_counter} moves!")
                     sleep(1)
-                    close()
+                    exit()
                 elif moves_counter == (Q * 5):
                     print(f"You have exceded the maximum number of movements permited for this match.")
                     user_movement = input("\nRestart -> 1\nExit -> 2   ")
@@ -200,7 +170,7 @@ def main():
                     if user_movement == "1":
                         main()
                     elif user_movement == "2":
-                        close()
+                        exit()
                 else:
                     print(f"Controls: w, a, s, d")
                     print(f"Moves made: {moves_counter}")
@@ -223,17 +193,16 @@ def main():
                             move_left(board, num_rows, num_columns)
                             moves_counter += 1
                         elif move == "2":
-                            close()
+                            exit()
                         else:
                             sleep(0.5)
                             print("\nPlease enter a valid move.\n")
                             sleep(0.5)
-                    printer(board, num_rows, num_columns)
+                    printBoard(board, num_rows, num_columns)
         elif user_input == "2":
             rules()
         elif user_input == "3":
-            close()
+            exit()
 
-
-intro()
+tittle()
 main()
